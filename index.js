0557d7472bb8bafd322ca39b45eba7f4d9854100
Guillermo Nicrosi <erknardsieghartrage@gmail.com>
2024-08-14 17:54:53 -0300

    mapa y localidad

--- index.js
+++ index.js
@@ -1,63 +1,80 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let items = document.querySelectorAll('.slider .list .item')
-  let prevBtn = document.getElementById('prev')
-  let nextBtn = document.getElementById('next')
-  let lastPosition = items.length - 1
-  let firstPosition = 0
-  let active = 0
-  let intervalId
+document.addEventListener("DOMContentLoaded", () => {
+  let items = document.querySelectorAll(".slider .list .item");
+  let prevBtn = document.getElementById("prev");
+  let nextBtn = document.getElementById("next");
+  let lastPosition = items.length - 1;
+  let firstPosition = 0;
+  let active = 0;
+  let intervalId;
 
   const setSlider = () => {
-    let oldActive = document.querySelector('.slider .list .item.active')
-    if (oldActive) oldActive.classList.remove('active')
-    items[active].classList.add('active')
+    let oldActive = document.querySelector(".slider .list .item.active");
+    if (oldActive) oldActive.classList.remove("active");
+    items[active].classList.add("active");
     //
-    nextBtn.classList.remove('d-none')
-    prevBtn.classList.remove('d-none')
-    if (active === lastPosition) nextBtn.classList.add('d-none')
-    if (active === firstPosition) prevBtn.classList.add('d-none')
-  }
+    nextBtn.classList.remove("d-none");
+    prevBtn.classList.remove("d-none");
+    if (active === lastPosition) nextBtn.classList.add("d-none");
+    if (active === firstPosition) prevBtn.classList.add("d-none");
+  };
 
   const startAutoSlide = () => {
     intervalId = setInterval(() => {
-      active = (active + 1) % items.length
-      setSlider()
-    }, 4000) // Cambia la imagen cada 5 segundos
-  }
+      active = (active + 1) % items.length;
+      setSlider();
+    }, 4000); // Cambia la imagen cada 5 segundos
+  };
 
   const stopAutoSlide = () => {
-    clearInterval(intervalId)
-  }
+    clearInterval(intervalId);
+  };
 
   nextBtn.onclick = () => {
-    stopAutoSlide()
-    active = active + 1
-    if (active > lastPosition) active = firstPosition
-    setSlider()
-    startAutoSlide()
-  }
+    stopAutoSlide();
+    active = active + 1;
+    if (active > lastPosition) active = firstPosition;
+    setSlider();
+    startAutoSlide();
+  };
 
   prevBtn.onclick = () => {
-    stopAutoSlide()
-    active = active - 1
-    if (active < firstPosition) active = lastPosition
-    setSlider()
-    startAutoSlide()
-  }
+    stopAutoSlide();
+    active = active - 1;
+    if (active < firstPosition) active = lastPosition;
+    setSlider();
+    startAutoSlide();
+  };
 
-  setSlider()
-  startAutoSlide()
+  setSlider();
+  startAutoSlide();
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 
   // set diameter
   const setDiameter = () => {
-    let slider = document.querySelector('.slider')
-    let widthSlider = slider.offsetWidth
-    let heightSlider = slider.offsetHeight
-    let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2))
-    document.documentElement.style.setProperty('--diameter', diameter + 'px')
-  }
-  setDiameter()
-  window.addEventListener('resize', () => {
-    setDiameter()
-  })
-})
+    let slider = document.querySelector(".slider");
+    let widthSlider = slider.offsetWidth;
+    let heightSlider = slider.offsetHeight;
+    let diameter = Math.sqrt(
+      Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2)
+    );
+    document.documentElement.style.setProperty("--diameter", diameter + "px");
+  };
+  setDiameter();
+  window.addEventListener("resize", () => {
+    setDiameter();
+  });
+});
